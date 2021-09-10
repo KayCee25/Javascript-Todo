89f7ec2471d6f6b9f576cd7ffc5fc024876431ad
KayCee25 <kenny.max225@gmail.com>
2021-09-10 09:25:26 -0700

    first commit

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 
 let button = document.getElementById("submit")
 let text = document.getElementById("todoInput")
-
+let todos = document.getElementById("todo-list")
 
 function btn () {
-    if(text.value === ""){
+    let todo = text.value
+    if(todo === ""){
         alert("Input Todo")
     }
     else{
-        alert("I will " + text.value + "")
-        text.value = "" 
+
     }
+}
+function done (e) {
+    if (e.target.tagName == "LI"){
+        e.target.classList.toggle("checked")
+    }
+    
 }
 
 button.addEventListener("click", btn)
+
+todos.addEventListener("click", done)
